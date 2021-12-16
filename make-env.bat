4c6718cbc966825cddcf5a2bbf5487987f0037b7
pchandrasekaran <jackstapleton920@gmail.com>
2021-12-17 03:17:40 +0530

    Update Batch Files

--- make-env.bat
+++
@@ -1,6 +0,0 @@
-python -m pip install virtualenv
-
-python -m virtualenv venv
-
-start /MAX cmd /k "title CMD && cls && cd venv/Scripts && activate && cd .. && cd .. && pip install -r requirements.txt"
-
