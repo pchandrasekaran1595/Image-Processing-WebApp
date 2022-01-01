392c7dc6ea1728200092628f96b7ae35e2a66757
pchandrasekaran <jackstapleton920@gmail.com>
2022-01-02 04:23:03 +0530

    Heroku Update #3 - Initial Setup

--- Main/settings.py
+++ Main/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if DEBUG:
